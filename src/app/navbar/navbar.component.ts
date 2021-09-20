28dd1cbba7072084cef0410e3fdaad530a77d378
Manuelb1990 <manuell@bluewin.ch>
2021-09-20 18:15:44 +0200

    fix navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from '../footer/footer.component';
 export class NavbarComponent implements OnInit {
   transformPercent = '100%';
   overlayShow = 'd-none';
+  divanzeigen = false;
   constructor(){
     
   }
@@ -21,6 +22,7 @@ export class NavbarComponent implements OnInit {
     
     console.log(this.transformPercent) 
     this.overlayShow = '';
+    this.divanzeigen = true;
     this.transformPercent = '0%'
     console.log(this.transformPercent)
 
@@ -31,6 +33,7 @@ export class NavbarComponent implements OnInit {
     
     console.log(this.transformPercent) 
     this.overlayShow = 'd-none';
+    this.divanzeigen = false;
     this.transformPercent = '100%'
     console.log(this.transformPercent)
 

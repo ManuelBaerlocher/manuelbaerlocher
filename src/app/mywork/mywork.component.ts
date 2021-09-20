c0aadf78ebe592fd31dbf540cd131eff138e6b2e
Manuelb1990 <manuell@bluewin.ch>
2021-09-20 21:33:46 +0200

    start myWork Functions

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-mywork',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mywork.component.scss']
 })
 export class MyworkComponent implements OnInit {
+  myworks = [
+    {
+      'title': "EL Pollo Loco",
+      'text': "JavaScript -based jump and run game.",
+      'class': "Javascript",
+      'image': "../../assets/img/laptop.jpg"
+    },
+    {
+      'title': "My Page",
+      'text': "My Page build with Angular",
+      'class': "Angular",
+      'image': "../../assets/img/laptop.jpg"
+    },
+    {
+      'title': "Join",
+      'text': "Kanban Board",
+      'class': "Javascript",
+      'image': "../../assets/img/laptop.jpg"
+    },
+    {
+      'title': "eat.ch",
+      'text': "Side to order and deliverey eat",
+      'class': "Javascript",
+      'image': "../../assets/img/laptop.jpg"
+    },
+  ]
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+
   }
+  ngOnInit() {
+
+
+
+  }
+
+
 
 }
